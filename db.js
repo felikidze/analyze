180925ae4eca7af31dc09c92f99b491b5b3e4911
felik <shtol.art@yandex.ru>
2024-06-23 21:29:26 +0500

    pg in .env

--- db.js
+++ db.js
@@ -1,8 +1,11 @@
 import pg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const pool = new pg.Pool({
-  user: "postgres",
-  password: "advance",
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
   host: "localhost",
   port: "5432",
   database: "analyze",
